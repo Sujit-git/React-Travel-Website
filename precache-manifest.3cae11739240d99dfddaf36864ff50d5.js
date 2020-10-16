f14cc0db167a58d816905d2260abbb1bd8e5a379
Sujit <sujitkhopade2001@gmail.com>
2020-10-16 01:56:33 -0700

    Updates

---
+++ precache-manifest.3cae11739240d99dfddaf36864ff50d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db050ecec8dccf2036618c580ca9a200",
+    "url": "/React-Travel-Website/index.html"
+  },
+  {
+    "revision": "20fe4d5d9d395d54e47c",
+    "url": "/React-Travel-Website/static/css/main.bfa2da51.chunk.css"
+  },
+  {
+    "revision": "a21f0bcf1852522157c0",
+    "url": "/React-Travel-Website/static/js/2.7150c6da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Travel-Website/static/js/2.7150c6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20fe4d5d9d395d54e47c",
+    "url": "/React-Travel-Website/static/js/main.3a0a6130.chunk.js"
+  },
+  {
+    "revision": "59e7cc864c970a562a0f",
+    "url": "/React-Travel-Website/static/js/runtime-main.5a1f7528.js"
+  }
+]);
